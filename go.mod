b84470f279e903d2b906fce921d741c913948416
Nikhil Dange <nikdange.me@gmail.com>
2024-11-21 23:49:44 +0530

    login endpoint with jwt token

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module simple-web-server
 
 go 1.23.1
+
+require github.com/golang-jwt/jwt/v5 v5.2.1 // indirect
--- main.go
+++ main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5" // go get xyz
 )
+
+var jwtSecretKey = []byte("secret_key_123456789")
 
 func main() {
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/login", loginHandler)
 	http.HandleFunc("/image", imageHandler)
 	http.HandleFunc("/greet", greetHandler)
 	fmt.Println("Server is running & listening on port http://localhost:8080")
@@ -36,4 +43,42 @@ func greetHandler(w http.ResponseWriter, r *http.Request) {
 	response := fmt.Sprintf("Namaskar %s", name)
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(response))
+}
+
+type Credentials struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	var creds Credentials
+	err := json.NewDecoder(r.Body).Decode(&creds)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	if creds.Username != "admin" || creds.Password != "password" {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	expTime := time.Now().Add(5 * time.Minute)
+	claims := &jwt.RegisteredClaims{
+		Subject:   creds.Username,
+		ExpiresAt: jwt.NewNumericDate(expTime),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(jwtSecretKey)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]string{
+		"token": tokenString,
+	})
+
 }
